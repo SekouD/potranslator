cecb4759381d76ce38638ac8a91cea59ccb64c23
SekouD <sekou.diao@gmail.com>
2018-06-29 22:07:42 +0200

    Added docstrings stubs.

--- tests/test_potranslator.py
+++ tests/test_potranslator.py
@@ -23,17 +23,28 @@ class TestPoTranslator:
     test_languages = ('es', 'fr', 'it', 'pt', 'ro')
 
     def test_failed_translate_unsupported_language(self):
+        """
 
+        :return:
+        """
         with pytest.raises(ValueError):
             failed_translation = self.translator.translate(self.test_po_file, 'sp')
         return
 
     def test_failed_translate_auto_detect(self):
+        """
+
+        :return:
+        """
         with pytest.raises(ValueError):
             failed_translation = self.translator.translate(self.test_po_file)
         return
 
     def test_translate(self):
+        """
+
+        :return:
+        """
         translation = self.translator.translate(self.test_po_file, 'es')
         assert all([entry.msgstr != '' for entry in translation])
         if not is_python2:
@@ -43,6 +54,10 @@ class TestPoTranslator:
         return
 
     def test_translate_auto_save(self):
+        """
+
+        :return:
+        """
         modif_time = getmtime(self.test_po_file)
         translation = self.translator.translate(self.test_po_file, 'es', auto_save=True)
         last_modif_time = getmtime(self.test_po_file)
@@ -50,6 +65,10 @@ class TestPoTranslator:
         return
 
     def test_translate_all_locale(self):
+        """
+
+        :return:
+        """
         translations = self.translator.translate_all_locale()
         assert not 'sp' in translations
         assert all(k in translations for k in self.test_languages)
@@ -61,6 +80,10 @@ class TestPoTranslator:
         return
 
     def test_translate_from_pot(self):
+        """
+
+        :return:
+        """
         translations = self.translator.translate_from_pot(self.test_pot_file, target_langs=self.test_languages)
         assert all(k in translations for k in self.test_languages)
         if not is_python2:
@@ -73,6 +96,10 @@ class TestPoTranslator:
         return
 
     def test_translate_all_pot(self):
+        """
+
+        :return:
+        """
         translations = self.translator.translate_all_pot(target_langs=self.test_languages)
         assert all(k in translations['test-authors.pot'] for k in self.test_languages)
         assert all(k in translations['test-usage.pot'] for k in self.test_languages)
