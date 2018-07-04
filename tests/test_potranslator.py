984f1a04d4da8056074c8fd6b84d465c0fa24f80
SekouD <sekou.diao@gmail.com>
2018-07-05 00:07:26 +0200

    Updated tests for the cli.

--- tests/test_potranslator.py
+++ tests/test_potranslator.py
@@ -6,6 +6,8 @@
 import pytest
 import sys
 import os
+import re
+from textwrap import dedent
 from os.path import isfile, join, getmtime
 from os import remove
 
@@ -38,6 +40,23 @@ def temp(request, tmpdir):
         os.chdir(cwd)
     request.addfinalizer(fin)
     return temp
+
+
+@pytest.fixture(scope="function")
+def home_in_temp(request, tmpdir):
+    """change HOME environment variable to temporary location
+
+     To avoid real .transifexrc will be rewritten.
+     """
+    home = os.environ.get('HOME')
+    os.environ['HOME'] = tmpdir.strpath
+
+    def fin():
+        del os.environ['HOME']
+        if home:
+            os.environ['HOME'] = home
+    request.addfinalizer(fin)
+    return tmpdir
 
 
 class TestPoTranslator:
@@ -221,3 +240,135 @@ class TestCommandLine:
     def test_build(self, temp):
         result = runner.invoke(commands.build, ['--locale-dir', 'locale'])
         assert result.exit_code == 0
+
+
+class TestTransifexCMD:
+    def test_create_transifexrc(self, home_in_temp):
+        r1 = runner.invoke(commands.main,
+                           [
+                               'create-transifexrc',
+                               '--transifex-username', 'spam-id',
+                               '--transifex-password', 'egg-pw',
+                           ])
+        assert r1.exit_code == 0
+
+    def test_create_txconfig(self, home_in_temp, temp):
+        r1 = runner.invoke(commands.main, ['create-txconfig'])
+        assert r1.exit_code == 0
+
+    def test_update_txconfig_resources(self, home_in_temp, temp):
+        r1 = runner.invoke(commands.main, ['create-txconfig'])
+        assert r1.exit_code == 0
+        r2 = runner.invoke(commands.update_txconfig_resources,
+                           [
+                               '--transifex-project-name', 'ham-project',
+                               '-d', 'locale',
+                           ])
+        assert r2.exit_code == 0
+
+    def test_update_txconfig_resources_with_config(self, home_in_temp, temp):
+        tx_dir = temp / '.tx'
+        tx_dir.makedirs()
+        (tx_dir / 'config').write_text(dedent("""\
+        [main]
+        host = https://www.transifex.com
+
+        [ham-project.domain1]
+        """))
+
+        (temp / '_build' / 'locale').copytree(temp / 'locale' / 'pot')
+
+        r1 = runner.invoke(commands.main, ['update-txconfig-resources', '-d', 'locale'])
+        assert r1.exit_code == 0
+
+        data = (tx_dir / 'config').text()
+        assert re.search(r'\[ham-project\.README\]', data)
+
+    def test_update_txconfig_resources_with_pot_dir_argument(self, home_in_temp, temp):
+        tx_dir = temp / '.tx'
+        tx_dir.makedirs()
+        (tx_dir / 'config').write_text(dedent("""\
+        [main]
+        host = https://www.transifex.com
+
+        [ham-project.domain1]
+        """))
+
+        r1 = runner.invoke(commands.main,
+                           ['update-txconfig-resources',
+                            '-d', 'locale',
+                            '-p', '_build/locale',
+                            ])
+        assert r1.exit_code == 0
+
+        data = (tx_dir / 'config').text().replace('\\', '/')
+        assert re.search(r'\[ham-project\.README\]', data)
+        assert re.search(r'source_file = _build/locale/README.pot', data)
+
+    def test_update_txconfig_resources_with_project_name_including_dots(self, home_in_temp, temp):
+        tx_dir = temp / '.tx'
+        tx_dir.makedirs()
+        (tx_dir / 'config').write_text(dedent("""\
+        [main]
+        host = https://www.transifex.com
+        """))
+
+        (temp / '_build' / 'locale').copytree(temp / 'locale' / 'pot')
+
+        r1 = runner.invoke(commands.main,
+                           ['update-txconfig-resources',
+                            '-d', 'locale',
+                            '--transifex-project-name', 'ham-project.com',
+                            ])
+        assert r1.exit_code == 0
+
+        data = (tx_dir / 'config').text()
+        assert re.search(r'\[ham-projectcom\.README\]', data)
+
+    def test_update_txconfig_resources_with_project_name_including_spaces(self, home_in_temp, temp):
+        tx_dir = temp / '.tx'
+        tx_dir.makedirs()
+        (tx_dir / 'config').write_text(dedent("""\
+        [main]
+        host = https://www.transifex.com
+        """))
+
+        (temp / '_build' / 'locale').copytree(temp / 'locale' / 'pot')
+
+        r1 = runner.invoke(commands.main,
+                           ['update-txconfig-resources',
+                            '-d', 'locale',
+                            '--transifex-project-name', 'ham project com',
+                            ])
+        assert r1.exit_code == 0
+
+        data = (tx_dir / 'config').text()
+        assert re.search(r'\[ham-project-com\.README\]', data)
+
+    def test_update_txconfig_resources_with_potfile_including_symbols(self, home_in_temp, temp):
+        tx_dir = temp / '.tx'
+        tx_dir.makedirs()
+        (tx_dir / 'config').write_text(dedent("""\
+        [main]
+        host = https://www.transifex.com
+        """))
+
+        (temp / '_build' / 'locale').copytree(temp / 'locale' / 'pot')
+
+        # copy README.pot to 'example document.pot'
+        readme = (temp / '_build' / 'locale' / 'README.pot').text()
+        (temp / '_build' / 'locale' / 'example document.pot').write_text(readme)
+
+        # copy README.pot to 'test.document.pot'
+        (temp / '_build' / 'locale' / 'test.document.pot').write_text(readme)
+
+        r1 = runner.invoke(commands.main,
+                           ['update-txconfig-resources',
+                            '-d', 'locale',
+                            '--transifex-project-name', 'ham project com',
+                            ])
+        assert r1.exit_code == 0
+
+        data = (tx_dir / 'config').text()
+        assert re.search(r'\[ham-project-com\.example_document\]', data)
+        assert re.search(r'\[ham-project-com\.test_document\]', data)
