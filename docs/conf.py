ae3d7e729265fdff187d22c6791e1c0939d51ba1
SekouD <sekou.diao@gmail.com>
2018-07-04 16:40:52 +0200

    Added locale.

--- docs/conf.py
+++ docs/conf.py
@@ -66,6 +66,9 @@ release = potranslator.__version__
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = None
+# Localization settings
+locale_dirs = ['locale/']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
