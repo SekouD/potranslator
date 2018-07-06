ca71f31e73b6e93a0cad4592aabf0e1817bad7cc
SekouD <sekou.diao@gmail.com>
2018-07-06 14:34:53 +0200

    Bump version: 1.0.3 → 1.0.4

--- potranslator/__init__.py
+++ potranslator/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """SekouD"""
 __email__ = 'sekoud.python@gmail.com'
-__version__ = '1.0.3'
+__version__ = '1.0.4'
 __copyright__ = "Copyright (c) 2017, SekouD"
 __credits__ = ("SekouD",)
 __license__ = "BSD"
--- setup.py
+++ setup.py
@@ -90,6 +90,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/SekouD/potranslator',
-    version='1.0.3',
+    version='1.0.4',
     zip_safe=False,
 )
