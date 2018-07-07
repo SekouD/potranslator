3228bb53c6922bde6b615125deda6bf4b46ce2f7
SekouD <sekoud.python@gmail.com>
2018-07-07 11:08:56 +0200

    Updated conf.py.

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,9 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+
+APP_DIR = os.path.normpath(os.path.join(os.getcwd(), '../..'))
+sys.path.insert(0, APP_DIR)
 
 import potranslator
 
