297a6b896929c5aa8b9aabd816f33afd455ecb93
SekouD <sekoud.python@gmail.com>
2018-07-08 19:20:45 +0200

    Updated type annotations for pycompat.py.

--- potranslator/pycompat.pyi
+++ potranslator/pycompat.pyi
@@ -1,10 +1,12 @@
 from typing import Text, Any, Optional, Mapping, Tuple, List, Sequence, Callable, Iterable, Dict
 
 FS_ENCODING: Text
-relpath: Text
 convert_with_2to3: Optional[Text]
 
 
 def execfile_(filepath: Text,
               _globals:Mapping
               ) -> None: ...
+
+def relpath(path: Text,
+            start: Text) -> Text: ...
